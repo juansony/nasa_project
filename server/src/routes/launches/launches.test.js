8073ce9ccdb2cb209af72a1a0c44375f500fccd7
epolanco97 <polanco.eric97@gmail.com>
2022-10-30 23:26:59 -0400

    CI pipeline complete flow

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,14 +1,16 @@
 const request = require("supertest");
 const app = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+const { loadPlanetsData } = require("../../models/planets.model");
 
 describe("Launch api test", () => {
   beforeAll(async () => {
-    mongoConnect();
+    await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
-    mongoDisconnect();
+    await mongoDisconnect();
   })
 
   describe("test GET /launches", () => {
